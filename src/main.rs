64a86e6d4419ab563964f16baf6548944650e031
Xavier Cambar <xcambar@gmail.com>
2017-09-15 13:27:33 +0200

    Displays short paths

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,14 @@ use regex::Regex;
 use std::env;
 use ansi_term::Colour::{Cyan, Blue, Purple};
 
+fn first_char(s: &str) -> String {
+    let chars: Vec<char> = s.chars().collect();
+    return match chars.len() {
+        0 => String::from(""),
+        _ => chars[0].to_string()
+    }
+}
+
 fn fmt_current_path(cwd: &str) -> String {
     let home: Regex = match env::home_dir() {
         Some(path) => Regex::new(path.to_str().unwrap()).unwrap(),
@@ -14,7 +22,7 @@ fn fmt_current_path(cwd: &str) -> String {
     let friendly_path = home.replace(cwd, "~").to_string();
     let mut friendly_path_split: Vec<&str> = friendly_path.split("/").collect();
     let current_dir = friendly_path_split.pop().unwrap().to_string();
-    let mut short_path: Vec<String> = friendly_path_split.iter().map(|s| s.chars().next().unwrap().to_string()).collect();
+    let mut short_path: Vec<String> = friendly_path_split.iter().map(|s| first_char(s)).collect();
     short_path.push(current_dir);
     short_path.join("/")
 }
