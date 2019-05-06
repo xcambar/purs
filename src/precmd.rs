221a4129a7d66cd228981a0119b25ecfc1dea6ef
Timm Behner <timm.behner@fkie.fraunhofer.de>
2019-05-06 14:25:55 +0200

    Bumped git2 version and made the required changes

--- src/precmd.rs
+++ src/precmd.rs
@@ -107,21 +107,21 @@ fn count_files_statuses(r: &Repository) -> Option<(usize, usize, usize, usize)> 
   Some((
     count_files(
       &statuses,
-      git2::STATUS_INDEX_NEW |
-      git2::STATUS_INDEX_MODIFIED |
-      git2::STATUS_INDEX_DELETED |
-      git2::STATUS_INDEX_RENAMED |
-      git2::STATUS_INDEX_TYPECHANGE
+      git2::Status::INDEX_NEW |
+      git2::Status::INDEX_MODIFIED |
+      git2::Status::INDEX_DELETED |
+      git2::Status::INDEX_RENAMED |
+      git2::Status::INDEX_TYPECHANGE
     ),
     count_files(
       &statuses,
-      git2::STATUS_WT_MODIFIED |
-      git2::STATUS_WT_DELETED |
-      git2::STATUS_WT_TYPECHANGE |
-      git2::STATUS_WT_RENAMED
+      git2::Status::WT_MODIFIED |
+      git2::Status::WT_DELETED |
+      git2::Status::WT_TYPECHANGE |
+      git2::Status::WT_RENAMED
     ),
-    count_files(&statuses, git2::STATUS_CONFLICTED),
-    count_files(&statuses, git2::STATUS_WT_NEW),
+    count_files(&statuses, git2::Status::CONFLICTED),
+    count_files(&statuses, git2::Status::WT_NEW),
   ))
 }
 
