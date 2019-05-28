d5d94829affdd8909060826fec5757beb4f7fd04
Lzu Tao <taolzu@gmail.com>
2019-05-28 23:58:19 +0700

    Rustfmt whole project

--- src/prompt.rs
+++ src/prompt.rs
@@ -1,38 +1,34 @@
-use clap::{ArgMatches, App, SubCommand, Arg};
+use clap::{App, Arg, ArgMatches, SubCommand};
 
-const INSERT_SYMBOL:&str = "❯";
-const COMMAND_SYMBOL:&str = "⬢";
-const COMMAND_KEYMAP:&str = "vicmd";
-const NO_ERROR:&str = "0";
+const INSERT_SYMBOL: &str = "❯";
+const COMMAND_SYMBOL: &str = "⬢";
+const COMMAND_KEYMAP: &str = "vicmd";
+const NO_ERROR: &str = "0";
 
 pub fn display(sub_matches: &ArgMatches<'_>) {
-  let last_return_code = sub_matches.value_of("last_return_code").unwrap_or("0");
-  let keymap = sub_matches.value_of("keymap").unwrap_or("US");
-  
-  let symbol = match keymap {
-    COMMAND_KEYMAP => COMMAND_SYMBOL,
-    _ => INSERT_SYMBOL,
-  };
+    let last_return_code = sub_matches.value_of("last_return_code").unwrap_or("0");
+    let keymap = sub_matches.value_of("keymap").unwrap_or("US");
 
-  let shell_color = match (symbol, last_return_code) {
-    (COMMAND_SYMBOL, _) => 3,
-    (_, NO_ERROR) => 5,
-    _ => 9,
-  };
+    let symbol = match keymap {
+        COMMAND_KEYMAP => COMMAND_SYMBOL,
+        _ => INSERT_SYMBOL,
+    };
 
-  print!("%F{{{}}}{}%f ", shell_color, symbol);
+    let shell_color = match (symbol, last_return_code) {
+        (COMMAND_SYMBOL, _) => 3,
+        (_, NO_ERROR) => 5,
+        _ => 9,
+    };
+
+    print!("%F{{{}}}{}%f ", shell_color, symbol);
 }
 
 pub fn cli_arguments<'a>() -> App<'a, 'a> {
-  SubCommand::with_name("prompt")
-    .arg(
-      Arg::with_name("last_return_code")
-        .short("r")
-        .takes_value(true)
-    )
-    .arg(
-      Arg::with_name("keymap")
-        .short("k")
-        .takes_value(true)
-    )
+    SubCommand::with_name("prompt")
+        .arg(
+            Arg::with_name("last_return_code")
+                .short("r")
+                .takes_value(true),
+        )
+        .arg(Arg::with_name("keymap").short("k").takes_value(true))
 }
