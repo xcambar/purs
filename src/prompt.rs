fd80f9e144b7ac4b9dd572d61b20334041101d05
Lzu Tao <taolzu@gmail.com>
2019-05-28 23:45:41 +0700

    Force 2018 edition idioms

--- src/prompt.rs
+++ src/prompt.rs
@@ -5,7 +5,7 @@ const COMMAND_SYMBOL:&str = "⬢";
 const COMMAND_KEYMAP:&str = "vicmd";
 const NO_ERROR:&str = "0";
 
-pub fn display(sub_matches: &ArgMatches) {
+pub fn display(sub_matches: &ArgMatches<'_>) {
   let last_return_code = sub_matches.value_of("last_return_code").unwrap_or("0");
   let keymap = sub_matches.value_of("keymap").unwrap_or("US");
   let venv_name = sub_matches.value_of("venv").unwrap_or("");
