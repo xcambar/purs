4183bfc96fd5edb3310f0e0ef7cfcabfb4553136
Lzu Tao <taolzu@gmail.com>
2019-06-05 01:27:02 +0700

    Run rustfmt on whole project

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,19 @@
 extern crate try_opt;
 use clap::{App, AppSettings};
 
-mod prompt;
 mod precmd;
+mod prompt;
 
 fn main() {
     let matches = App::new("Purs")
-      .setting(AppSettings::SubcommandRequired)
-      .subcommand(precmd::cli_arguments())
-      .subcommand(prompt::cli_arguments())
-      .get_matches();
+        .setting(AppSettings::SubcommandRequired)
+        .subcommand(precmd::cli_arguments())
+        .subcommand(prompt::cli_arguments())
+        .get_matches();
 
     match matches.subcommand() {
-      ("precmd", Some(sub_matches)) => precmd::display(sub_matches),
-      ("prompt", Some(sub_matches)) => prompt::display(sub_matches),
-      _ => (),
+        ("precmd", Some(sub_matches)) => precmd::display(sub_matches),
+        ("prompt", Some(sub_matches)) => prompt::display(sub_matches),
+        _ => (),
     }
 }
--- src/prompt.rs
+++ src/prompt.rs
@@ -1,49 +1,41 @@
-use clap::{ArgMatches, App, SubCommand, Arg};
+use clap::{App, Arg, ArgMatches, SubCommand};
 
-const INSERT_SYMBOL:&str = "❯";
-const COMMAND_SYMBOL:&str = "⬢";
-const COMMAND_KEYMAP:&str = "vicmd";
-const NO_ERROR:&str = "0";
+const INSERT_SYMBOL: &str = "❯";
+const COMMAND_SYMBOL: &str = "⬢";
+const COMMAND_KEYMAP: &str = "vicmd";
+const NO_ERROR: &str = "0";
 
 pub fn display(sub_matches: &ArgMatches<'_>) {
-  let last_return_code = sub_matches.value_of("last_return_code").unwrap_or("0");
-  let keymap = sub_matches.value_of("keymap").unwrap_or("US");
-  let venv_name = sub_matches.value_of("venv").unwrap_or("");
+    let last_return_code = sub_matches.value_of("last_return_code").unwrap_or("0");
+    let keymap = sub_matches.value_of("keymap").unwrap_or("US");
+    let venv_name = sub_matches.value_of("venv").unwrap_or("");
 
-  let symbol = match keymap {
-    COMMAND_KEYMAP => COMMAND_SYMBOL,
-    _ => INSERT_SYMBOL,
-  };
+    let symbol = match keymap {
+        COMMAND_KEYMAP => COMMAND_SYMBOL,
+        _ => INSERT_SYMBOL,
+    };
 
-  let shell_color = match (symbol, last_return_code) {
-    (COMMAND_SYMBOL, _) => 3,
-    (_, NO_ERROR) => 5,
-    _ => 9,
-  };
+    let shell_color = match (symbol, last_return_code) {
+        (COMMAND_SYMBOL, _) => 3,
+        (_, NO_ERROR) => 5,
+        _ => 9,
+    };
 
-  let venv = match venv_name.len() {
-    0 => String::from(""),
-    _ => format!("%F{{11}}|{}|%f ", venv_name),
-  };
+    let venv = match venv_name.len() {
+        0 => String::from(""),
+        _ => format!("%F{{11}}|{}|%f ", venv_name),
+    };
 
-  print!("{}%F{{{}}}{}%f ", venv, shell_color, symbol);
+    print!("{}%F{{{}}}{}%f ", venv, shell_color, symbol);
 }
 
 pub fn cli_arguments<'a>() -> App<'a, 'a> {
-  SubCommand::with_name("prompt")
-    .arg(
-      Arg::with_name("last_return_code")
-        .short("r")
-        .takes_value(true)
-    )
-    .arg(
-      Arg::with_name("keymap")
-        .short("k")
-        .takes_value(true)
-    )
-    .arg(
-      Arg::with_name("venv")
-        .long("venv")
-        .takes_value(true)
-    )
+    SubCommand::with_name("prompt")
+        .arg(
+            Arg::with_name("last_return_code")
+                .short("r")
+                .takes_value(true),
+        )
+        .arg(Arg::with_name("keymap").short("k").takes_value(true))
+        .arg(Arg::with_name("venv").long("venv").takes_value(true))
 }
